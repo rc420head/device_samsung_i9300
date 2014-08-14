e1681c6f65d421baace2bcc108b3142a03306983
rc420head <dennislebhardt@yahoo.com>
2014-08-14 11:47:46 -0500

    Add the sweetness

--- slim.mk
+++ slim.mk
@@ -6,17 +6,17 @@ TARGET_SCREEN_WIDTH := 720
 $(call inherit-product, device/samsung/i9300/full_i9300.mk)
 
 # Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+$(call inherit-product, vendor/candykat/config/common_full_phone.mk)
 
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/slim/config/gsm.mk)
+$(call inherit-product, vendor/candykat/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := i9300
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9300
-PRODUCT_NAME := slim_i9300
+PRODUCT_NAME := candykat_i9300
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I9300
 PRODUCT_MANUFACTURER := samsung
